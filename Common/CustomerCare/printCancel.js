a8084645560fa191147f1df13951cff99fa47db5
mallik482 <malli.paidala@gmail.com>
2015-12-10 18:46:25 +0530

    Adding project

---
+++ Common/CustomerCare/printCancel.js
@@ -0,0 +1,3 @@
+app.controller('CancelCtrl', function ($scope) {
+
+});
---
+++ Common/NavBar/nav.js
@@ -0,0 +1,10 @@
+app.directive("ngNavBar", function () { 
+    return {
+        restrict:'E',
+        templateUrl:'Common/NavBar/navbar.html',
+        controller:function($scope){
+            
+        }
+    }
+
+});
